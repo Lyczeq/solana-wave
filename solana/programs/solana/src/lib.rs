c4501b0d30c98fa4efea1e0af00f7f112c033239
Lyczeq <lyczeq@gmail.com>
2022-08-20 13:05:31 +0200

    Added a logic and tests that counts the waves on the program

--- solana/programs/solana/src/lib.rs
+++ solana/programs/solana/src/lib.rs
@@ -7,14 +7,55 @@ declare_id!("Fg6PaFpoGXkYsidMpWTK6W2BeZ7FEfcYkg476zPFsLnS");
 // everything below this line is our program, we'll create some handlers to communicate with the program
 // it's a macro that attach code to the module, it's like "inheriting" a class
 #[program]
-pub mod solana { // module declaration, similar to a class from OOP
-    use super::*; // inheriting? 
+pub mod solana {
+    // module declaration, similar to a class from OOP
+
+    use super::*; // inheriting?
 
     // an initialization function, that takes a function and return Ok which is a Result type
     pub fn initialize(ctx: Context<Initialize>) -> Result<()> {
+        // Get a mutable reference to the base account. It allows to change the base_account instead of working on a local copy.
+        let base_account = &mut ctx.accounts.base_account;
+
+        // Initialize total_messages property
+        base_account.total_waves = 0;
+
+        Ok(())
+    }
+
+    pub fn add_wave(ctx: Context<AddWave>) -> Result<()> {
+        // Get a reference to the account and increment total_messages.
+        let base_account = &mut ctx.accounts.base_account;
+        base_account.total_waves += 1;
         Ok(())
     }
 }
 
 #[derive(Accounts)]
-pub struct Initialize {}
+pub struct Initialize<'info> {
+    // we tell SOLANA how we want to initialize the BaseAccount
+    // init - create a new account owned by the current program
+    // payer = user - who pays for the account to be created, in this case the user
+    // it allocates 9000 bytes of space for the account.
+    #[account(init, payer = user, space = 9000)]
+    pub base_account: Account<'info, BaseAccount>,
+    #[account(mut)]
+    // Data passed to the program that proves that the user calling this program owns their wallet account.
+    pub user: Signer<'info>,
+
+    //Reference to the SystemProgram which basically runs SOLANA. It creates accounts on SOLANA.
+    pub system_program: Program<'info, System>,
+}
+
+// Create Context that has access to a mutable reference to base_account.
+#[derive(Accounts)]
+pub struct AddWave<'info> {
+    #[account(mut)] // Access to a mutable reference to base_account
+    pub base_account: Account<'info, BaseAccount>,
+}
+
+// It tells the program what kind of account it can make and what to hold inside of it. Here, BaseAccount holds one thing and it's an unsigned integer.
+#[account]
+pub struct BaseAccount {
+    pub total_waves: u64,
+}
--- solana/tests/solana.js
+++ solana/tests/solana.js
@@ -1,20 +1,47 @@
 const anchor = require('@project-serum/anchor');
+const { SystemProgram } = anchor.web3;
 
 const main = async () => {
-  console.log('🚀 Starting test...');
+  console.log('Starting test');
 
   // we say anchor to set the provider which takes data from `solana config get`, it takes the local variable. That's how anchor knows to run the code locally
-  anchor.setProvider(anchor.AnchorProvider.env());
+  const provider = anchor.AnchorProvider.env();
+  anchor.setProvider(provider);
 
   /* we want to use the program called 'solana'. It compiles the code in lib.rs and deploys it on local validator. 
   Note: Naming + folder structure is mega important here. Ex. Anchor knows to look at programs/solana/src/lib.rs b/c we used anchor.workspace.Solana.
   */
   const program = anchor.workspace.Solana;
 
-  //We call the initialize fn from the 'solana' program. We need to wait for it, because local validator has to 'mine' the instruction.
-  const tx = await program.rpc.initialize();
+  // Create credentials for our program to use.
+  const baseAccount = anchor.web3.Keypair.generate();
+
+  //We call the initialize fn from the 'solana' program. We need to wait for it, because local validator has to 'mine' the instruction. It's necessary to pass the needed params specified in the Initialize struct.
+  const tx = await program.rpc.initialize({
+    accounts: {
+      baseAccount: baseAccount.publicKey,
+      user: provider.wallet.publicKey,
+      systemProgram: SystemProgram.programId,
+    },
+    signers: [baseAccount],
+  });
 
   console.log('📝 Your transaction signature', tx);
+
+  let accountData = await program.account.baseAccount.fetch(
+    baseAccount.publicKey
+  );
+  console.log('Waves count', accountData.totalWaves.toString());
+
+  // Call add_wave fn from the program
+  await program.rpc.addWave({
+    accounts: {
+      baseAccount: baseAccount.publicKey,
+    },
+  });
+
+  accountData = await program.account.baseAccount.fetch(baseAccount.publicKey);
+  console.log('Waves count', accountData.totalWaves.toString());
 };
 
 const runMain = async () => {
