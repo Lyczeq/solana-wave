75964491d3e30898e341b7d4db386b080250fab6
Jakub Lyczko <lyczeq@gmail.com>
2022-08-20 13:42:15 +0200

    Added Solana programs (#2)
    * Added test and notes for solana
    
    * Added a note about the rent
    
    * Added a logic and tests that counts the waves on the program
    
    * Added a handler to add a wave to the solana program. Wrote tests for that

--- solana/programs/solana/src/lib.rs
+++ solana/programs/solana/src/lib.rs
@@ -1,15 +1,79 @@
+// use works like an import, we import things from the anchor library
 use anchor_lang::prelude::*;
 
+// program id, anchor generated that, info for solana how to run the program
 declare_id!("Fg6PaFpoGXkYsidMpWTK6W2BeZ7FEfcYkg476zPFsLnS");
 
+// everything below this line is our program, we'll create some handlers to communicate with the program
+// it's a macro that attach code to the module, it's like "inheriting" a class
 #[program]
 pub mod solana {
-    use super::*;
+    // module declaration, similar to a class from OOP
 
+    use super::*; // inheriting?
+
+    // an initialization function, that takes a function and return Ok which is a Result type
     pub fn initialize(ctx: Context<Initialize>) -> Result<()> {
+        // Get a mutable reference to the base account. It allows to change the base_account instead of working on a local copy.
+        let base_account = &mut ctx.accounts.base_account;
+
+        // Initialize total_messages property
+        base_account.total_waves = 0;
+
+        Ok(())
+    }
+
+    pub fn add_wave(ctx: Context<AddWave>, wave: String) -> Result<()> {
+        // Get a reference to the account and increment total_messages.
+        let base_account = &mut ctx.accounts.base_account;
+        let user = &mut ctx.accounts.user;
+
+        let new_wave = Wave {
+            wave: wave.to_string(),
+            user_address: *user.to_account_info().key,
+        };
+
+        base_account.waves_list.push(new_wave);
+        base_account.total_waves += 1;
         Ok(())
     }
 }
 
 #[derive(Accounts)]
-pub struct Initialize {}
+pub struct Initialize<'info> {
+    // we tell SOLANA how we want to initialize the BaseAccount
+    // init - create a new account owned by the current program
+    // payer = user - who pays for the account to be created, in this case the user
+    // it allocates 9000 bytes of space for the account.
+    #[account(init, payer = user, space = 9000)]
+    pub base_account: Account<'info, BaseAccount>,
+    #[account(mut)]
+    // Data passed to the program that proves that the user calling this program owns their wallet account.
+    pub user: Signer<'info>,
+
+    //Reference to the SystemProgram which basically runs SOLANA. It creates accounts on SOLANA.
+    pub system_program: Program<'info, System>,
+}
+
+// Create Context that has access to a mutable reference to base_account.
+#[derive(Accounts)]
+pub struct AddWave<'info> {
+    #[account(mut)] // Access to a mutable reference to base_account
+    pub base_account: Account<'info, BaseAccount>,
+    #[account(mut)]
+    pub user: Signer<'info>,
+}
+
+// It tells Anchor how to serialize/deserialize the struct. Data is stored in an "account", it's just a file and we serialize the data into a binary format before storing it.
+#[derive(Debug, Clone, AnchorSerialize, AnchorDeserialize)]
+pub struct Wave {
+    pub wave: String,
+    pub user_address: Pubkey,
+}
+
+// It tells the program what kind of account it can make and what to hold inside of it. Here, BaseAccount holds one thing and it's an unsigned integer.
+#[account]
+pub struct BaseAccount {
+    pub total_waves: u64,
+    pub waves_list: Vec<Wave>,
+}
--- solana/tests/solana.js
+++ solana/tests/solana.js
@@ -1,13 +1,79 @@
-const anchor = require("@project-serum/anchor");
+const anchor = require('@project-serum/anchor');
+const { SystemProgram } = anchor.web3;
+const deepEqualInAnyOrder = require('deep-equal-in-any-order');
+const chai = require('chai');
 
-describe("solana", () => {
-  // Configure the client to use the local cluster.
-  anchor.setProvider(anchor.AnchorProvider.env());
+chai.use(deepEqualInAnyOrder);
+const { expect } = chai;
+const mockData = (wave, userAddress) => {
+  return [
+    {
+      wave,
+      userAddress,
+    },
+  ];
+};
 
-  it("Is initialized!", async () => {
-    // Add your test here.
-    const program = anchor.workspace.Solana;
-    const tx = await program.methods.initialize().rpc();
-    console.log("Your transaction signature", tx);
+const main = async () => {
+  console.log('Starting test');
+
+  // we say anchor to set the provider which takes data from `solana config get`, it takes the local variable. That's how anchor knows to run the code locally
+  const provider = anchor.AnchorProvider.env();
+  anchor.setProvider(provider);
+
+  /* we want to use the program called 'solana'. It compiles the code in lib.rs and deploys it on local validator. 
+  Note: Naming + folder structure is mega important here. Ex. Anchor knows to look at programs/solana/src/lib.rs b/c we used anchor.workspace.Solana.
+  */
+  const program = anchor.workspace.Solana;
+
+  // Create credentials for our program to use.
+  const baseAccount = anchor.web3.Keypair.generate();
+
+  //We call the initialize fn from the 'solana' program. We need to wait for it, because local validator has to 'mine' the instruction. It's necessary to pass the needed params specified in the Initialize struct.
+  const tx = await program.rpc.initialize({
+    accounts: {
+      baseAccount: baseAccount.publicKey,
+      user: provider.wallet.publicKey,
+      systemProgram: SystemProgram.programId,
+    },
+    signers: [baseAccount],
   });
-});
+
+  console.log('📝 Your transaction signature', tx);
+
+  let accountData = await program.account.baseAccount.fetch(
+    baseAccount.publicKey
+  );
+  expect(accountData.totalWaves.toString()).equals('0');
+
+  const waveMessage = 'abcd';
+
+  // Call add_wave fn from the program
+  await program.rpc.addWave(waveMessage, {
+    accounts: {
+      baseAccount: baseAccount.publicKey,
+      user: provider.wallet.publicKey,
+    },
+  });
+
+  accountData = await program.account.baseAccount.fetch(baseAccount.publicKey);
+
+  const { wavesList, totalWaves } = accountData;
+  expect(totalWaves.toString()).equals('1');
+
+  expect(wavesList).to.deep.equalInAnyOrder(
+    mockData(waveMessage, provider.wallet.publicKey)
+  );
+};
+
+const runMain = async () => {
+  try {
+    await main();
+    process.exit(0);
+  } catch (error) {
+    console.error(error);
+    process.exit(1);
+  }
+};
+
+runMain();
