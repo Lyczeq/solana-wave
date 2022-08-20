688d9b23fd06ab781673748f98643d15d1772a12
Jakub Lyczko <lyczeq@gmail.com>
2022-08-20 19:03:44 +0200

    Client interaction with prorgram (#4)
    * Copied the idl file to the client
    
    * Added a anchor and web3 packages to interact with solana
    
    * Modified a vite config to work with the anchor library
    
    * Added helper functions
    
    * Simplified App js and added reset styles
    
    * Added styling to the Waves component and logic to fetch waves

--- client/src/components/Waves/Waves.jsx
+++ client/src/components/Waves/Waves.jsx
@@ -1,62 +1,129 @@
+import { Program } from '@project-serum/anchor';
+import { PublicKey } from '@solana/web3.js';
+import copy from 'copy-to-clipboard';
 import React, { useEffect, useState } from 'react';
+import { createWaveAccount, getBaseAccount, getProvider } from '../../helpers';
 import { Button } from '../Button/Button';
+
+import idl from '../../idl.json';
 import './Waves.css';
 
-const TEST_GIFS = [
-  'https://i.giphy.com/media/eIG0HfouRQJQr1wBzz/giphy.webp',
-  'https://media3.giphy.com/media/L71a8LW2UrKwPaWNYM/giphy.gif?cid=ecf05e47rr9qizx2msjucl1xyvuu47d7kf25tqt2lvo024uo&rid=giphy.gif&ct=g',
-  'https://media4.giphy.com/media/AeFmQjHMtEySooOc8K/giphy.gif?cid=ecf05e47qdzhdma2y3ugn32lkgi972z9mpfzocjj6z1ro4ec&rid=giphy.gif&ct=g',
-  'https://i.giphy.com/media/PAqjdPkJLDsmBRSYUp/giphy.webp',
-];
+let baseAccount = getBaseAccount();
+
+// Get our program's id from the IDL file.
+const programID = new PublicKey(idl.metadata.address);
 
 export const Waves = ({ walletAddress }) => {
   const [userMessage, setUserMessage] = useState('');
-  const [messages, setMessages] = useState([]);
-
+  const [wavesList, setWavesList] = useState([]);
+  const [errorMessage, setErrorMessage] = useState('');
   const handleMessageChange = event => {
     const { value } = event.target;
     setUserMessage(value);
   };
 
-  const sendMessage = async () => {
-    if (userMessage.length > 0) {
-      setMessages([userMessage, ...messages]);
+  const sendWave = async () => {
+    if (!userMessage.length) {
+      alert('The message cannot be empty!');
+      return;
+    }
+
+    try {
+      const provider = getProvider();
+      // @ts-ignore
+      const program = new Program(idl, programID, provider);
+
+      await program.rpc.addWave(userMessage, {
+        accounts: {
+          baseAccount: baseAccount.publicKey,
+          user: provider.wallet.publicKey,
+        },
+      });
+
+      await getWavesList();
       setUserMessage('');
-    } else {
-      console.log('Empty input. Try again.');
+    } catch (e) {
+      console.log('Error sending a Wave:', e);
+    }
+  };
+
+  const getWavesList = async () => {
+    try {
+      setErrorMessage('');
+      const provider = getProvider();
+      // @ts-ignore
+      const program = new Program(idl, programID, provider);
+      const account = await program.account.baseAccount.fetch(
+        baseAccount.publicKey
+      );
+
+      // @ts-ignore
+      setWavesList(account.wavesList);
+    } catch (error) {
+      setErrorMessage('Something went wrong, try again later!');
+      // @ts-ignore
+      setWavesList(null);
     }
   };
 
   useEffect(() => {
     if (walletAddress) {
-      console.log('Fetching messages...');
-
-      setMessages(TEST_GIFS);
+      
+      getWavesList();
     }
   }, [walletAddress]);
+
+  // If we hit this, it means the program account hasn't been initialized.
+  if (wavesList === null) {
+    return (
+      <div className="connected-container">
+        <button
+          className="cta-button submit-wave-button"
+          onClick={() => createWaveAccount(getWavesList)}
+        >
+          Do One-Time Initialization For Wave Program Account
+        </button>
+      </div>
+    );
+  }
 
   return (
     <div className="connected-container">
       <form
+        className="message-form"
         onSubmit={event => {
           event.preventDefault();
-          sendMessage();
+          sendWave();
         }}
       >
         <input
+          className="wave-input"
           type="text"
           placeholder="Enter your message!"
           value={userMessage}
           onChange={handleMessageChange}
         />
-        <Button type="submit" className="submit-gif-button">
+        <Button type="submit" className="submit-wave-button">
           Submit
         </Button>
+        {errorMessage && <p className="error-message">{errorMessage}</p>}
       </form>
-      <div className="gif-grid">
-        {messages.map(gif => (
-          <div className="gif-item" key={gif}>
-            <img src={gif} alt={gif} />
+      <div className="wave-grid">
+        {(wavesList || []).map(({ userAddress, wave }) => (
+          <div className="wave-item" key={wave}>
+            {console.log(userAddress)}
+            <p className="user-address-heading">
+              👋 Address:
+              <span
+                onClick={() => copy(userAddress.toString())}
+                className="user-address"
+              >
+                {userAddress.toString()}
+              </span>
+            </p>
+            <div className="wave-content">
+              <p>{wave}</p>
+            </div>
           </div>
         ))}
       </div>
--- client/src/helpers.js
+++ client/src/helpers.js
@@ -1,21 +1,59 @@
-export const checkWalletConnection = async () => {
-  try {
+import { AnchorProvider, Program, web3 } from '@project-serum/anchor';
+import { clusterApiUrl, Connection, PublicKey } from '@solana/web3.js';
+
+import idl from './idl.json';
+import keypair from './keypair.json';
+
+// SystemProgram is a reference to the Solana runtime!
+const { SystemProgram, Keypair } = web3;
+
+// Get our program's id from the IDL file.
+const programID = new PublicKey(idl.metadata.address);
+
+// Set our network to devnet.
+const network = clusterApiUrl('devnet');
+
+// Controls how we want to acknowledge when a transaction is "done".
+const opts = {
+  preflightCommitment: 'processed',
+};
+
+export const getProvider = () => {
+  // @ts-ignore
+  const connection = new Connection(network, opts.preflightCommitment);
+  const provider = new AnchorProvider(
+    connection,
     // @ts-ignore
-    const { solana } = window;
-    console.log(solana);
-    if (solana) {
-      if (solana.isPhantom) {
-        console.log('Phantom wallet found!');
-        const response = await solana.connect({ onlyIfTrusted: true });
-        console.log(
-          'Connected with Public Key:',
-          response.publicKey.toString()
-        );
-      }
-    } else {
-      alert('Solana object not found! Get a Phantom Wallet 👻');
-    }
+    window.solana,
+    // @ts-ignore
+    opts.preflightCommitment
+  );
+  return provider;
+};
+
+export const createWaveAccount = async getWaves => {
+  try {
+    const provider = getProvider();
+    // @ts-ignore
+    const program = new Program(idl, programID, provider);
+    const baseAccount = getBaseAccount();
+    await program.rpc.initialize({
+      accounts: {
+        baseAccount: baseAccount.publicKey,
+        user: provider.wallet.publicKey,
+        systemProgram: SystemProgram.programId,
+      },
+      signers: [baseAccount],
+    });
+
+    await getWaves();
   } catch (error) {
-    console.error(error);
+    console.log('Error creating BaseAccount account:', error);
   }
+};
+
+export const getBaseAccount = () => {
+  const arr = Object.values(keypair._keypair.secretKey);
+  const secret = new Uint8Array(arr);
+  return web3.Keypair.fromSecretKey(secret);
 };
