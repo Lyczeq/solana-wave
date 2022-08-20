95d0fb8c1698ec56b02ccb61a5a23d8e3297041f
Lyczeq <lyczeq@gmail.com>
2022-08-20 11:59:55 +0200

    Added test and notes for solana

--- solana/programs/solana/src/lib.rs
+++ solana/programs/solana/src/lib.rs
@@ -1,11 +1,16 @@
+// use works like an import, we import things from the anchor library
 use anchor_lang::prelude::*;
 
+// program id, anchor generated that, info for solana how to run the program
 declare_id!("Fg6PaFpoGXkYsidMpWTK6W2BeZ7FEfcYkg476zPFsLnS");
 
+// everything below this line is our program, we'll create some handlers to communicate with the program
+// it's a macro that attach code to the module, it's like "inheriting" a class
 #[program]
-pub mod solana {
-    use super::*;
+pub mod solana { // module declaration, similar to a class from OOP
+    use super::*; // inheriting? 
 
+    // an initialization function, that takes a function and return Ok which is a Result type
     pub fn initialize(ctx: Context<Initialize>) -> Result<()> {
         Ok(())
     }
--- solana/tests/solana.js
+++ solana/tests/solana.js
@@ -1,13 +1,30 @@
-const anchor = require("@project-serum/anchor");
+const anchor = require('@project-serum/anchor');
 
-describe("solana", () => {
-  // Configure the client to use the local cluster.
+const main = async () => {
+  console.log('🚀 Starting test...');
+
+  // we say anchor to set the provider which takes data from `solana config get`, it takes the local variable. That's how anchor knows to run the code locally
   anchor.setProvider(anchor.AnchorProvider.env());
 
-  it("Is initialized!", async () => {
-    // Add your test here.
-    const program = anchor.workspace.Solana;
-    const tx = await program.methods.initialize().rpc();
-    console.log("Your transaction signature", tx);
-  });
-});
+  /* we want to use the program called 'solana'. It compiles the code in lib.rs and deploys it on local validator. 
+  Note: Naming + folder structure is mega important here. Ex. Anchor knows to look at programs/solana/src/lib.rs b/c we used anchor.workspace.Solana.
+  */
+  const program = anchor.workspace.Solana;
+
+  //We call the initialize fn from the 'solana' program. We need to wait for it, because local validator has to 'mine' the instruction.
+  const tx = await program.rpc.initialize();
+
+  console.log('📝 Your transaction signature', tx);
+};
+
+const runMain = async () => {
+  try {
+    await main();
+    process.exit(0);
+  } catch (error) {
+    console.error(error);
+    process.exit(1);
+  }
+};
+
+runMain();
